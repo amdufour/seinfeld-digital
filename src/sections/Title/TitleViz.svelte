2e02fbec27ce16f21e8b6c241380ee803e85c1b0
Anne-Marie Dufour <annema.dufour@gmail.com>
2024-11-11 12:41:42 +0100

    Move episode viz to the left side of the screen

--- src/sections/Title/TitleViz.svelte
+++ src/sections/Title/TitleViz.svelte
@@ -12,8 +12,24 @@
   $: svgWidth = innerWidth >= gridContainer
     ? gridContainer - (innerWidth - gridContainer) / 2
     : innerWidth - 2 * padding
+
+  /**
+	 * @type {number}
+	 */
+  $: marginLeft = innerWidth >= gridContainer
+    ? (innerWidth - gridContainer) / 2
+    : padding
 </script>
 
 <svelte:window bind:innerWidth />
 
-<svg width={svgWidth} height="132"></svg>
+<div style="margin-left: -{marginLeft}px">
+  <svg width={svgWidth} height="132">
+    <!-- use each statement here! -->
+    <rect class="fill-s1" x={0} y={0} width={500} height={12} />
+    <rect class="fill-s2" x={0} y={15} width={500} height={12} />
+    <rect class="fill-s3" x={0} y={2*15} width={500} height={12} />
+    <rect class="fill-s4" x={0} y={3*15} width={500} height={12} />
+    <rect class="fill-s5" x={0} y={4*15} width={500} height={12} />
+  </svg>
+</div>
