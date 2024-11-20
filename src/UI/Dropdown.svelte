be676a036bd484333e014469bdcfed40ea62e668
Anne-Marie Dufour <annema.dufour@gmail.com>
2024-11-20 08:54:49 +0100

    Add native dropdown

--- src/UI/Dropdown.svelte
+++ src/UI/Dropdown.svelte
@@ -1,76 +1,33 @@
 <script>
-  import { createPopper } from "@popperjs/core";
-  import UpDownCarets from "../icons/UpDownCarets.svelte";
+  import UpDownCarets from "$lib/assets/UpDownCarets.svg";
 
-  let { options, value, prefix } = $props();
-  let dropdownPopoverShow = $state(false);
-
-  /**
-	 * @type {Element | import("@popperjs/core").VirtualElement}
-	 */
-  let btnDropdownRef;
-  /**
-	 * @type {HTMLElement}
-	 */
-  let popoverDropdownRef;
-
-  const toggleDropdown = () => {
-    if (dropdownPopoverShow) {
-      dropdownPopoverShow = false;
-    } else {
-      dropdownPopoverShow = true;
-      createPopper(btnDropdownRef, popoverDropdownRef, {
-        placement: "bottom-start",
-      });
-    }
-  };
+  let { options, value, prefix = $bindable() } = $props();
 </script>
 
-<!-- TODO: Use native select on mobile and tablet -->
-<div class="flex flex-wrap">
-  <div class="w-full sm:w-6/12 md:w-4/12 px-4">
-    <div class="relative inline-flex align-middle w-full">
-      <button 
-        class="dropdown flex items-center font-semibold text-2xl pl-2 pr-1 py-1 rounded shadow outline-none focus:outline-none ease-linear transition-all duration-150" 
-        type="button" 
-        bind:this="{btnDropdownRef}"
-        onclick="{toggleDropdown}"
-      >
-        <span>{prefix}{value}</span><span class="ml-1"><UpDownCarets /></span>
-      </button>
-      <div 
-        bind:this="{popoverDropdownRef}" 
-        class="text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1 min-w-20 
-        {dropdownPopoverShow ? 'block':'hidden'}"
-      >
-        <ul class="py-1">
-          {#each options as option}
-            <li>
-              <button class={`dropdown-option ${option === value ? 'active' : ''}`}>{prefix}{option}</button>
-            </li>
-          {/each}
-        </ul>
-      </div>
-    </div>
-  </div>
+<div class="dropdown">
+  <select 
+    bind:value={value}
+    style="background-image: url({UpDownCarets});"
+  >
+    {#each options as option}
+      <option value={option}>{prefix}{option}</option>
+    {/each}
+  </select>
 </div>
 
 <style>
-  .dropdown {
+  .dropdown select {
+    appearance: none;
+    outline: none;
+    min-width: 80px;
+    padding: 0px 8px;
+    font-size: 1.5625rem;
+    font-weight: 600;
+    background-color: transparent;
+    background-repeat: no-repeat;
+    background-size: auto 27px;
+    background-position: right 4px center;
     border: 2px solid #E71D80;
     border-radius: 5px;
-  }
-  .dropdown-option {
-    width: 100%;
-    padding: 4px 16px;
-    text-align: left;
-    transition: color 150ms ease-out;
-  }
-  .dropdown-option:hover,
-  .dropdown-option.active {
-    color: #E71D80;
-  }
-  .dropdown-option.active:hover {
-    cursor: default;
   }
 </style>
