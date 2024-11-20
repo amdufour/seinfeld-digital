d5747e39f872d582ca6ab1b9d2f6c6a28e7398df
Anne-Marie Dufour <annema.dufour@gmail.com>
2024-11-20 10:13:50 +0100

    Handle random episode

--- src/sections/catalog/EpisodeControls.svelte
+++ src/sections/catalog/EpisodeControls.svelte
@@ -3,6 +3,7 @@
   import Dropdown from "../../UI/Dropdown.svelte";
   import Prev from "../../icons/Prev.svelte";
   import Next from "../../icons/Next.svelte";
+  import Random from "../../icons/Random.svelte";
 
   let { episodes, currentSeason = $bindable(), currentEpisode = $bindable() } = $props()
   const seasonNums = seasons.map(s => s.seasonNum)
@@ -29,6 +30,12 @@
     } else {
       currentEpisode = currentEpisode + 1
     }
+  }
+
+  const handleGoToRandomEpisode = () => {
+    currentSeason = Math.floor(Math.random() * 9) + 1
+    const numEpisodes = episodes.filter((/** @type {{ season: any; }} */ e) => e.season === currentSeason).map((/** @type {{ episode: any; }} */ e) => e.episode).length
+    currentEpisode = Math.floor(Math.random() * numEpisodes) + 1
   }
 </script>
 
@@ -68,4 +75,22 @@
   >
     <Next color={currentSeason === 9 && currentEpisode === 24 ? "#BEBABC" : "#E71D80"} />
   </button>
+
+  <!-- Random episode -->
+  <div class="flex items-center">
+    <div class="small">OR</div>
+    <button class="flex items-center ml-3" onclick={handleGoToRandomEpisode}>
+      <div class="icon flex items-center justify-center"><Random /></div>
+      <div class="small accent ml-2 w-28 text-left leading-tight">Go to a random episode</div>
+    </button>
+  </div>
 </div>
+
+<style>
+  .icon {
+    width: 36px;
+    height: 36px;
+    background-color: #E71D80;
+    border-radius: 50%;
+  }
+</style>
