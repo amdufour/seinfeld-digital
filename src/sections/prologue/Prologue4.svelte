724481713ff603c4743146df1eae8298fd39dea0
Anne-Marie Dufour <annema.dufour@gmail.com>
2024-11-29 09:37:55 +0100

    Add scrub animations

--- src/sections/prologue/Prologue4.svelte
+++ src/sections/prologue/Prologue4.svelte
@@ -15,13 +15,14 @@
 	import BookCatalog2 from '$lib/assets/book/book_catalog2.png';
 
 	let innerWidth = $state(1600);
+	let innerHeight = $state(800);
 
 	onMount(() => {
 		// Pin text and book cover
 		ScrollTrigger.create({
 			trigger: '#books-inner-container',
 			start: 'bottom bottom',
-			pin: '#book-cover'
+			pin: '#book-cover-container'
 		});
 
 		// Handle horizontal scroll
@@ -42,9 +43,24 @@
 					scrub: 1
 				}
 			});
-			imgSliderTimeline.to(imgSlider, {
-				x: -calculateSliderX
-			});
+			imgSliderTimeline
+				.to(imgSlider, {
+					x: -calculateSliderX
+				})
+				.to(
+					'#book-cover',
+					{
+						scale: 1.2
+					},
+					0
+				)
+				.from(
+					'#accent-line',
+					{
+						width: 0
+					},
+					0
+				);
 		}
 
 		// Smooth scroll
@@ -62,39 +78,63 @@
 	});
 </script>
 
-<svelte:window bind:innerWidth />
+<svelte:window bind:innerWidth bind:innerHeight />
 
 <div id="book-section-container" class="flex h-screen flex-col justify-between">
-	<div id="book-cover" class="container grid grid-cols-1 lg:grid-cols-2" style="margin-top: 15%;">
+	<div
+		id="book-cover-container"
+		class="container grid grid-cols-1 lg:grid-cols-2"
+		style="margin-top: {innerHeight / 6}px;"
+	>
 		<div class="col-span-1 flex h-full items-center px-4">
 			<p>This project, prolonging the portfolio book published in 2020, explores both.</p>
 		</div>
 		<div class="col-span-1 px-4">
-			<img src={BookCover} alt="Cover of the book The Seinfeld Chronicles." />
+			<img id="book-cover" src={BookCover} alt="Cover of the book The Seinfeld Chronicles." />
 		</div>
 	</div>
 	<div id="books-outer-container">
+		<div id="accent-line" class="mb-3 h-2 w-full" style="background-color: #E71D80;"></div>
 		<div id="books-inner-container" class="flex">
-			<img class="mx-4 h-80" src={BookIntro} alt="Introduction of the book." />
-			<img class="mx-4 h-80" src={BookCalendar} alt="Calendar of the nine seasons of Seinfeld." />
 			<img
-				class="mx-4 h-80"
+				class="mx-4"
+				style="height: {innerHeight / 3.5}px;"
+				src={BookIntro}
+				alt="Introduction of the book."
+			/>
+			<img
+				class="mx-4"
+				style="height: {innerHeight / 3.5}px;"
+				src={BookCalendar}
+				alt="Calendar of the nine seasons of Seinfeld."
+			/>
+			<img
+				class="mx-4"
+				style="height: {innerHeight / 3.5}px;"
 				src={BookLaughs}
 				alt="Data visualizations of the laughs caused by the four main characters."
 			/>
 			<img
-				class="mx-4 h-80"
+				class="mx-4"
+				style="height: {innerHeight / 3.5}px;"
 				src={BookScatterplot}
 				alt="Data visualizations of the peak performances of the four main characters."
 			/>
-			<img class="mx-4 h-80" src={BookQuotes} alt="Famous quotes from each episode." />
 			<img
-				class="mx-4 h-80"
+				class="mx-4"
+				style="height: {innerHeight / 3.5}px;"
+				src={BookQuotes}
+				alt="Famous quotes from each episode."
+			/>
+			<img
+				class="mx-4"
+				style="height: {innerHeight / 3.5}px;"
 				src={BookCatalog1}
 				alt="Data visualization of season 4 episode 11 'The Contest'."
 			/>
 			<img
-				class="mx-4 h-80"
+				class="mx-4"
+				style="height: {innerHeight / 3.5}px;"
 				src={BookCatalog2}
 				alt="Data visualization of season 7 episode 6 'The Soup Nazi'."
 			/>
