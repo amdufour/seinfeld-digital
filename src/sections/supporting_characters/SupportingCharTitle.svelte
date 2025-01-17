93440b672aa7a36749c1fda7b7e320edcae24ac4
Anne-Marie Dufour <annema.dufour@gmail.com>
2025-01-17 10:46:37 +0100

    Play video on mouse over

--- src/sections/supporting_characters/SupportingCharTitle.svelte
+++ src/sections/supporting_characters/SupportingCharTitle.svelte
@@ -30,17 +30,54 @@
 	const tilesWidth = $derived(innerWidth / numColumns);
 	const tilesHeight = $derived(innerHeight / numRows);
 	$inspect(tiles);
+
+	// Play video on hover
+	const handlePlayVideo = (/** @type {string} */ tileId) => {
+		/**
+		 * @type {HTMLVideoElement | null}
+		 */
+		const video = document.querySelector(`#${tileId} video`);
+		video?.play();
+	};
+	const handlePauseVideo = (/** @type {string} */ tileId) => {
+		/**
+		 * @type {HTMLVideoElement | null}
+		 */
+		const video = document.querySelector(`#${tileId} video`);
+		video?.pause();
+	};
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
 <div class="tiles flex flex-wrap">
 	{#each tiles as tile}
-		<div class="tile-container relative">
+		<div
+			id={tile.id}
+			class="tile-container relative"
+			role="group"
+			onfocus={() => handlePlayVideo(tile.id)}
+			onmouseover={() => handlePlayVideo(tile.id)}
+			onmouseleave={() => handlePauseVideo(tile.id)}
+		>
 			<div
 				class="tile"
 				style="width: {tilesWidth}px; height: {tilesHeight}px; background-image: url('https://amdufour.github.io/hosted-data/apis/thumbnails/{tile.thumbnail}');"
 			></div>
+			<!-- svelte-ignore a11y_media_has_caption -->
+			<video
+				class="absolute bottom-0 bottom-0 left-0 right-0"
+				playsinline
+				preload="auto"
+				data-object-fit="cover"
+				style="width: {tilesWidth}px; height: {tilesHeight}px;"
+				poster="https://amdufour.github.io/hosted-data/apis/thumbnails/{tile.thumbnail}"
+			>
+				<source
+					src="https://amdufour.github.io/hosted-data/apis/videos/{tile.video}"
+					type="video/mp4"
+				/>
+			</video>
 			<div class="info absolute bottom-0 left-0 right-0">
 				<div
 					class="details px-4"
@@ -60,16 +97,21 @@
 </div>
 
 <style>
+	.tile,
+	.overlay,
+	.info .details {
+		transition: all 300ms ease-out;
+	}
 	.tile {
 		background-repeat: no-repeat;
 		background-position: center;
 		background-size: cover;
+		opacity: 1;
 	}
 	.overlay {
 		height: 100%;
 		max-height: 100%;
 		opacity: 0.7;
-		transition: max-height 300ms ease-out;
 	}
 	.info {
 		height: 100%;
@@ -83,10 +125,12 @@
 		max-height: 0;
 		overflow: hidden;
 		font-size: 1.125rem;
-		transition: all 300ms ease-out;
 	}
 	.info .details .name {
 		font-weight: 600;
+	}
+	.tile-container:hover .tile {
+		opacity: 0;
 	}
 	.tile-container:hover .overlay {
 		max-height: 0;
