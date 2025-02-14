46c0cb0545a08fbd2a220620c1ac6e15c81a1d24
Anne-Marie Dufour <annema.dufour@gmail.com>
2025-02-14 08:49:18 +0100

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	base: "/seinfeld-digital/",
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		outDir: 'build' // Ensure this matches what your GitHub Actions uses
+	}
 });
