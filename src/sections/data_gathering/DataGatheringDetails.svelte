5ece25be3889be009e4805d53b7daa83403e170d
Anne-Marie Dufour <annema.dufour@gmail.com>
2025-05-14 12:16:34 +0200

    Add parallax

--- src/sections/data_gathering/DataGatheringDetails.svelte
+++ src/sections/data_gathering/DataGatheringDetails.svelte
@@ -4,12 +4,14 @@
 	import { gsap } from 'gsap';
 	import { ScrollTrigger } from 'gsap/ScrollTrigger';
 	gsap.registerPlugin(ScrollTrigger);
+    import Lenis from 'lenis';
 
     import Laugh from '../../icons/Laugh.svelte';
     import tv_noise from '$lib/assets/tv_noise.png';
+    import { soundIsAuth } from '../../stores/soundAuthStore'
 
     let innerWidth = $state(1600);
-	let sideSpacing = $derived(innerWidth >= 1280 ? (innerWidth - 1280) / 2 + 16 + 25 : 30)
+	let sideSpacing = $derived(innerWidth >= 1280 ? (innerWidth - 1280) / 2 + 16 + 25 : 32)
 
     /**
 	 * @type {Tone.Players}
@@ -26,10 +28,11 @@
 	 */
     let playLoop;
     const playLaughs = () => {
-        laughsTrack.start();
+        if ($soundIsAuth && laughsTrack) {
+            laughsTrack.start();
+        }
 
         const pauseDuration = Math.floor(Math.random() * (10000 - 5000 + 1) + 5000);
-        console.log(pauseDuration)
         playLoop = setTimeout(() => {
             playLaughs();
         }, pauseDuration);
@@ -119,6 +122,34 @@
                 ease: 'power3.out'
             }, "-=2");
 
+        // Add parallax effect to images
+		let images = gsap.utils.toArray('.data-gathering-parallax');
+        console.log(images)
+		images.forEach((image) => {
+			const speed = image.dataset.speed;
+			gsap.to(image, {
+				yPercent: speed * 50,
+				ease: 'none',
+				scrollTrigger: {
+					trigger: image,
+					start: 'top bottom',
+					scrub: true
+				}
+			});
+		});
+
+		// Smooth scroll
+		const lenis = new Lenis();
+
+		/**
+		 * @param {number} time
+		 */
+		function raf(time) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
     });
 </script>
 
@@ -127,39 +158,43 @@
 <div class="w-screen" style="margin-left: -25px;">
     <div class="container py-96">
         <div id="data-gathering-1" class="grid grid-cols-12 mb-48">
-            <div class="col-span-6 relative">
+            <div class="col-span-10 md:col-span-6 relative">
                 <div class="mask">
                     <div class="text">It doesn’t take a genius to recognise the main goal of a sitcom is to offer situational comedy. It also stands to reason that a reliable indicator of a situation comedy being funny is to measure the reaction of an audience’s laughter through the laugh track. These days it is rare to find laugh tracks on modern sitcoms, but Seinfeld had one and with the show always being filmed in front of a <span class="highlight pl-1">live studio audience</span> - with any scenes filmed outside played back in the studio - and so the laughter heard is authentic.</div>
                 </div>
-                <div class="absolute w-20 md:w-32 h-20 md:h-32" style="top: -68px; right: -130px;">
+                <div class="absolute w-20 md:w-32 h-20 md:h-32" style="top:-68px; right: {innerWidth >= 768 ? -130 : -50}px;">
                     <Laugh />
                 </div>
             </div>
-            <div class="col-span-6"></div>
-            <div class="col-span-4"></div>
-            <div class="col-span-8 mt-8" style="margin-right: -{sideSpacing}px;">
-                <img src="https://amdufour.github.io/hosted-data/apis/images/audience.jpg" alt="Jerry Seinfeld talking with the audience during taping." />
-                <div class="number text pt-2">Source: <a href="https://www.facebook.com/story.php?story_fbid=539096318663826&id=100076903884453" target="_blank">The Seinfeld World</a></div>
+            <div class="col-span-2 md:col-span-6"></div>
+            <div class="col-span-2 md:col-span-4"></div>
+            <div class="col-span-10 md:col-span-8 mt-8" style="margin-right: -{sideSpacing}px;">
+                <img class="data-gathering-parallax" src="https://amdufour.github.io/hosted-data/apis/images/audience.jpg" alt="Jerry Seinfeld talking with the audience during taping." data-speed="1.2" />
+                <div class="number text pt-2" style="background-color: rgba(249, 245, 247, 0.6);">Source: <a href="https://www.facebook.com/story.php?story_fbid=539096318663826&id=100076903884453" target="_blank">The Seinfeld World</a></div>
             </div>
-            <!-- <audio src="https://amdufour.github.io/hosted-data/apis/videos/laugh.mp3" autoplay loop></audio> -->
         </div>
 
         <div id="data-gathering-2" class="grid grid-cols-12 mb-48">
-            <div class="col-span-1"></div>
+            <div class="col-auto md:col-span-1"></div>
             <div class="col-span-7" style="margin-right: -50px;">
-                <img src="https://amdufour.github.io/hosted-data/apis/images/data_gathering.png" alt="Data spreadsheet and tv during data gathering." />
+                <img class="data-gathering-parallax" src="https://amdufour.github.io/hosted-data/apis/images/data_gathering.png" alt="Data spreadsheet and tv during data gathering." data-speed="0.7" />
             </div>
-            <div class="col-span-4 mask">
+            <div class="col-span-4 md:col-auto"></div>
+            <div class="col-span-2 md:col-auto"></div>
+            <div class="col-span-10 md:col-span-4 relative z-10 mask" style="background-color: rgba(249, 245, 247, 0.6);">
                 <p class="text">We gathered all the data for this project by watching every Seinfeld episode and <span class="highlight">manually logging</span> the events and classifications established to serve the intention of the analysis.</p>
                 <p class="text">Laughter has a spectrum of levels, from the subtle smile characteristic of ‘inner’ laughter, through to more external titters, chuckles, chortles, and through to belly laughs or howls. To establish a standard measurement any laughter heard during the episodes counted as simply as laughter, regardless of whether it was loud or fleeting.</p>
             </div>
         </div>
 
         <div id="data-gathering-3" class="grid grid-cols-12">
-            <div class="col-span-8 mask">
-                <div class="text">For consistency each distinct moment of laughter was allocated to distinct units of <span class="highlight">5-second durations</span>. When testing out the data collection approach over three sample episodes, the 5-second duration proved to be the most reliable and representative ‘average’ duration, from the gag’s delivery to the audience’s laughter subsiding. Occasionally, laughter would run for longer than 5 seconds, sometimes persisting for 10 and even 15 seconds. In these rare cases, each 5-second unit would count as a laughter moment.</div>
+            <div class="col-span-12 md:col-span-8 mask">
+                <div class="text">
+                    <p>For consistency each distinct moment of laughter was allocated to distinct units of <span class="highlight">5-second durations</span>. When testing out the data collection approach over three sample episodes, the 5-second duration proved to be the most reliable and representative ‘average’ duration, from the gag’s delivery to the audience’s laughter subsiding.</p>
+                    <p>Occasionally, laughter would run for longer than 5 seconds, sometimes persisting for 10 and even 15 seconds. In these rare cases, each 5-second unit would count as a laughter moment.</p>
+                </div>
             </div>
-            <div class="col-span-12">
+            <div class="col-span-12" style="margin-left: {innerWidth >= 768 ? 0 : -sideSpacing + 25}px; margin-right: {innerWidth >= 768 ? 0 : -sideSpacing}px;">
                 <!-- svelte-ignore a11y_media_has_caption -->
 				 <div class="relative video-container" data-speed="1">
 					<video playsinline autoplay loop muted>
