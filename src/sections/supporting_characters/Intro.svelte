6c40371e154ce045536bda6b181e19a429b73353
Anne-Marie Dufour <annema.dufour@gmail.com>
2025-05-28 15:58:24 +0200

    Add supporting characters screen 2

--- src/sections/supporting_characters/Intro.svelte
+++ src/sections/supporting_characters/Intro.svelte
@@ -1,9 +1,12 @@
 <script>
   import { onMount } from 'svelte';
 	import { gsap } from 'gsap/dist/gsap';
+	import Lenis from 'lenis';
+	import { soundIsAuth } from '../../stores/soundAuthStore';
+	import tv_noise from '$lib/assets/tv_noise.png';
 
   onMount(() => {
-    gsap.set('#supporting_chars_screen_1 p', { translateY: 100, opacity: 0 });
+    gsap.set('#supporting_chars p', { translateY: 100, opacity: 0 });
 
     const tl1 = gsap.timeline({
       scrollTrigger: {
@@ -26,7 +29,70 @@
         delay: 1,
         ease: 'power3.out'
       }, '<-0.5');
+
+    const tl2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#supporting_chars_screen_2',
+        start: 'top top<20%'
+      }
     });
+    tl1
+      .to('#supporting_chars_screen_2 p', {
+        translateY: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'power3.out'
+      })
+      .to('#supporting_chars_screen_2 .highlight', {
+        webkitTextFillColor: 'transparent',
+        backgroundPosition: '200% center',
+        duration: 2,
+        delay: 1,
+        ease: 'power3.out'
+      }, '<-0.5');
+
+    // Add parallax effect to videos
+		let videos = gsap.utils.toArray('.parallax');
+		videos.forEach((video) => {
+			const speed = video.dataset.speed;
+			gsap.to(video, {
+				yPercent: speed * 50,
+				ease: 'none',
+				scrollTrigger: {
+					trigger: video,
+					start: 'top bottom',
+					scrub: true
+				}
+			});
+		});
+
+		// Smooth scroll
+		const lenis = new Lenis();
+
+		/**
+		 * @param {number} time
+		 */
+		function raf(time) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+  });
+
+  let video1IsMuted = $state(true);
+  let video2IsMuted = $state(true);
+  const handleVideoMouseEnter = (/** @type {number} */ video) => {
+    console.log(video)
+		if ($soundIsAuth) {
+			video1IsMuted = video === 1 ? false : true;
+			video2IsMuted = video === 2 ? false : true;
+		}
+	};
+	const handleVideoMouseLeave = (/** @type {{ target: any; }} */ e) => {
+		video1IsMuted = true;
+		video2IsMuted = true;
+	};
 </script>
 
 <div id="supporting_chars" class="bg-black text-white">
@@ -42,4 +108,90 @@
       </div>
     </div>
   </div>
+
+  <!-- Screen 2 -->
+  <div id="supporting_chars_screen_2" class="h-screen w-screen">
+    <div class="container">
+      <div class="grid grid-cols-12 gap-4">
+        <div class="col-span-7 h-screen flex flex-col justify-center">
+          <p>The main struggle was to strike a balance between creating enough distinct character categories, with sufficient individuality for those who warrant it, but not carving out too many that the list becomes excessive and unwieldy. For instance, recurring characters like Jerry and Kramer's neighbour, <span class="highlight">Newman</span>, or Elaine's boss, <span class="highlight">J. Peterman</span>, have a cult following that made it tempting to mark them out as distinct individual characters.</p>
+        </div>
+        <div class="col-span-5">
+          <div class="grid grid-cols-5 mt-16">
+            <div class="col-span-3">
+              <div
+                class={`parallax`}
+                data-speed={-0.5}
+                role="presentation"
+                onmouseenter={() => handleVideoMouseEnter(1)}
+                onmouseleave={handleVideoMouseLeave}
+              >
+                <!-- svelte-ignore a11y_media_has_caption -->
+                <video playsinline autoplay loop bind:muted={video1IsMuted}>
+                  <source
+                    src={`https://amdufour.github.io/hosted-data/apis/videos/${'17.Jerry_Newman'}.mp4`}
+                    type="video/mp4"
+                  />
+                </video>
+                <div class="readable-layer z-1 absolute bottom-0 left-0 right-0 top-0"></div>
+                <div
+                  class="absolute z-10 bottom-0 left-0 right-0 top-0"
+                  style="background-image: url('{tv_noise}')"
+                ></div>
+                <div class="episode">S8E2 - The Soulmate</div>
+              </div>
+            </div>
+            <div class="col-span-2"></div>
+            <div class="col-span-1"></div>
+            <div class="col-span-4 mt-16">
+              <div
+                class={`parallax`}
+                data-speed={-1.2}
+                role="presentation"
+                onmouseenter={() => handleVideoMouseEnter(2)}
+                onmouseleave={handleVideoMouseLeave}
+              >
+                <!-- svelte-ignore a11y_media_has_caption -->
+                <video playsinline autoplay loop bind:muted={video2IsMuted}>
+                  <source
+                    src={`https://amdufour.github.io/hosted-data/apis/videos/${'7a.Peterman'}.mp4`}
+                    type="video/mp4"
+                  />
+                </video>
+                <div class="readable-layer z-1 absolute bottom-0 left-0 right-0 top-0"></div>
+                <div
+                  class="absolute z-10 bottom-0 left-0 right-0 top-0"
+                  style="background-image: url('{tv_noise}')"
+                ></div>
+                <div class="episode">S9E18 - The Frogger</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>
+
+<style>
+	.parallax {
+		box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+	}
+	.episode {
+		position: absolute;
+		z-index: 2;
+		left: 5px;
+		bottom: 0;
+		font-size: 1.125rem;
+		line-height: 1.2;
+		font-weight: 600;
+		transform: translateY(15px);
+		opacity: 0;
+		transition: all 350ms cubic-bezier(0.165, 0.84, 0.44, 1);
+	}
+	.parallax:hover .episode {
+		transform: translateY(0);
+		opacity: 1;
+	}
+</style>
